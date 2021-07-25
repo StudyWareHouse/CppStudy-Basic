1d690cf51434d5540cbfc43235116c7a58c56ffb
winterguard <aliasbonfire@gmail.com>
2021-07-25 16:51:02 +0900

    create cpp project

--- cpp-basic/main.cpp
+++ cpp-basic/main.cpp
@@ -1,14 +1,9 @@
-//
-//  main.cpp
-//  cpp-basic
-//
-//  Created by Winterguard on 2021/07/25.
-//
-
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+
     return 0;
 }
