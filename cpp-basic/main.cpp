864fe48a7adc0336b040fd90c645367f98093ae7
winterguard <aliasbonfire@gmail.com>
2021-07-27 21:33:15 +0900

    calc

--- cpp-basic/main.cpp
+++ cpp-basic/main.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 int main()
 {
-    const int GREAT_VICTORY_SALSU = 612;
-    const int GREAT_VICTORY_GWIJU = 1019;
+    int one = 1;
+    int two = 2;
+    int three = 3;
+    int four = 4;
     
-    cout << "고구려 살수대첩: " << GREAT_VICTORY_SALSU << "년" << endl;
-    cout << "고려 귀주대첩: " << GREAT_VICTORY_GWIJU << "년" << endl;
+    cout << "1 + 3 = " << one + three << endl;
+    cout << "4 - 2 = " << four - two << endl;
+    cout << "2 * 3 = " << two * three << endl;
+    cout << "4 / 2 = " << four / two << endl;
+    
+    cout << "2 + 3 * 4 = " << two + three * four << endl;
+    cout << "1 + 4 / 2 = " << one + four / two << endl;
     
     return 0;
 }
